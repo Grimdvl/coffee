78ca1406fc490b619ca0c42af9ff4acc978a5e80
GrimdvlLaptop <grimdvl@gmail.com>
2024-06-11 11:33:15 +0300

    creating second page

--- src/components/our-coffee/our-coffee.js
+++ src/components/our-coffee/our-coffee.js
@@ -1,9 +1,12 @@
+import ourCoffeeBg from '../../assets/img/our-coffee-bg.jpg';
+
 import './our-coffee.css';
 
 const OurCoffee = () => {
     return (
         <section className="our-coffee">
-            
+            <div className="our-coffee-img"><img src={ourCoffeeBg} alt="our-coffee" /></div>
+            <h1 className='main-title'>Our coffee</h1>
         </section>
     );
 }
